4cc2254fa3309deb293caffe396b60547129093e
dlwlstks96 <72801465+dlwlstks96@users.noreply.github.com>
2022-04-01 22:47:36 +0900

    주문 기능 테스트 코드 작성

--- src/main/java/jpabook/jpashop/domain/Address.java
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -1,15 +1,25 @@
 package jpabook.jpashop.domain;
 
 import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.Embeddable;
 
 @Embeddable //jpa 내장 타입
-@Getter
+@Getter @Setter
 public class Address {
 
     private String city;
     private String street;
     private String zipCode;
 
+    public Address() {
+
+    }
+
+    public Address(String city, String street, String zipCode) {
+        this.city = city;
+        this.street = street;
+        this.zipCode = zipCode;
+    }
 }
